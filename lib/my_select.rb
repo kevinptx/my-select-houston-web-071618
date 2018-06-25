571f47fdd200395728573be6526063c9a40d498d
kevinptx <kevin.peery@gmail.com>
2018-06-25 13:13:42 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,11 @@
 def my_select(collection)
   i = 0
   new_arr = []
-  if collection.length > 0
   while i < collection.length
     if yield(collection[i])
       new_arr << collection[i]
     end
       i += 1
     end
-else
-  puts "Empty collection"
-end
-new_arr
+    new_arr
 end
